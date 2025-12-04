dd32fd52daa54f0ac28e2cf6e6752b7a15143443
Hung-497 <giahung092007@gmail.com>
2025-12-04 14:56:43 +0200

    Put everything Hung + Trung done to main

--- python/core.py
+++ python/core.py
@@ -29,17 +29,18 @@ letter_parts = {
     "LIRF": "You have a great chance to save the world.",
     "GCTS": "Do what you can.",
     "LFPG": "There are still computers left, scattered around the EU that can help you",
-    "EHEH": "to remember who you are.",
+    "EHEH": "To remember who you are.",
     "EDDF": "But don’t get too excited, since I tricked you; the secret base, the key, my password - remember?",
 }
 
 ## this is a function for defining random airports in r rows
 def get_airport(db_conf): #random 30 airports for the game
-    sql = """SELECT ident, name, municipality
+    sql = """SELECT ident, name, municipality, latitude_deg AS lat, longitude_deg AS lon
              FROM airport
              WHERE continent = 'EU'
                AND type = 'large_airport'
-             ORDER by RAND() LIMIT 30;"""
+             ORDER by RAND() 
+             LIMIT 30;"""
     con = mysql.connector.connect(**db_conf)
     cur = con.cursor(dictionary=True)
     cur.execute(sql)
@@ -67,12 +68,22 @@ class Coregame:
             "LFPG": "Charles de Gaulle International Airport",
             "EHEH": "Eindhoven Airport",
             "EDDF": "Frankfurt am Main Airport"}
+        fixed_coords = {
+        "LIRF": (41.8003, 12.2389),
+        "GCTS": (28.0445, -16.5725),
+        "LFPG": (49.0097, 2.5479),
+        "EHEH": (51.4501, 5.3745),
+        "EDDF": (50.0379, 8.5622),
+        }
         # Ensure fixed airports are included
         for icao in FIXED_CODE_AIRPORTS:
+            lat, lon = fixed_coords[icao]
             self.airport[icao] = {
                 "ident": icao,
                 "name": fixed_name.get(icao, ""),
-                "municipality": fixed_municipalities.get(icao, "")
+                "municipality": fixed_municipalities.get(icao, ""),
+                "lat": lat,
+                "lon": lon,
             }
         self.idents = list(self.airport)
         # Player state
@@ -88,9 +99,21 @@ class Coregame:
         self.outcome = None
 
     def built_message(self):
-        remaining = [i for i in self.idents if i not in self.visited]
+        remaining_airport = [{"code": code,
+                              "name": self.airport[code]["name"] + " - " + self.airport[code]["municipality"],
+                              "lat": self.airport[code]["lat"],
+                              "lon": self.airport[code]["lon"] } 
+                             for code in self.airport if code not in self.visited]
+        random.shuffle(remaining_airport)
+        curren_airport_infor = {
+            "code": self.cur,
+            "name": self.airport[self.cur]["name"] + " - " + self.airport[self.cur]["municipality"],
+            "lat": self.airport[self.cur]["lat"],
+            "lon": self.airport[self.cur]["lon"] 
+        }
         return {
             "current_airport": self.cur,
+            "current_airport_info": curren_airport_infor,
             "start_airport": self.start,
             "days_left": self.days_left,
             "max_days": self.max_days,
@@ -99,8 +122,8 @@ class Coregame:
             "game_over" : self.game_over,
             "outcome": self.outcome,
             "messages": self.messages,
-            "reamaining_airport_count": len(remaining),
-            "remaining_airports": sorted(remaining)
+            "reamaining_airport_count": len(remaining_airport),
+            "remaining_airports": remaining_airport,
         }
   
     def reset_messages(self):
@@ -127,11 +150,10 @@ class Coregame:
             return msg
         
         if dest_ident == self.cur:
-            msg = "You already at this place, please move to another destination."
+            msg = "You are already at this place, please move to another destination."
             self.add_message(msg)
             return msg
         
-        self.show_letter_so_far()
         self.days_left -= 1
         self.cur = dest_ident
         self.visited.add(dest_ident)
@@ -148,22 +170,18 @@ class Coregame:
 
         # If all letter parts are found
         if len(self.found) == len(FIXED_CODE_AIRPORTS):
-            msg += "\n\n>>> The letter is now complete! <<<"
             self.game_over = True
             self.outcome = "win"
 
         if self.days_left <= 0 and not self.win_condition():
-            msg += "\n\n>>> You have run out of days! <<<"
             self.game_over = True
             self.outcome = "lose"
         
         if self.game_over:
             if self.outcome == "win":
-                msg += "\n\nCongratulations! You have successfully completed your mission and saved the world!"
-                msg += "The game is already over. Please start a new game."
+                pass
             else:
-                msg += "\n\nUnfortunately, you have failed your mission. The world is lost."
-                msg += "The game is already over. Please start a new game."
+                pass
 
         self.add_message(msg)
         return msg
@@ -191,26 +209,53 @@ game = Coregame(db_conf)
 
 @app.route('/move/<dest_ident>')
 def move(dest_ident):
-    game.move(dest_ident.upper().strip())
-    state = game.built_message()
-    json_response = json.dumps(state)
-    return Response(response=json_response, status=200, mimetype="application/json")
-
+    try:
+        game.move(dest_ident.upper().strip())
+        state = game.built_message()
+        json_response = json.dumps(state)
+        return Response(response=json_response, status=200, mimetype="application/json")
+    except ValueError:
+        response = {
+            "message": "Invalid number as addend",
+            "status": 400
+        }
+        json_response = json.dumps(response)
+        http_response = Response(response=json_response, status=400, mimetype="application/json")
+        return http_response
+    
 @app.route('/state')
 def state():
-    if not game.messages:
-        game.add_message(f"You are now at {game.fmt(game.cur)}")
-    state = game.built_message()
-    json_response = json.dumps(state)
-    return Response(response=json_response, status=200, mimetype="application/json")
+    try:
+        if not game.messages:
+            game.add_message(f"You are now at {game.fmt(game.cur)}")
+        state = game.built_message()
+        json_response = json.dumps(state)
+        return Response(response=json_response, status=200, mimetype="application/json")
+    except ValueError:
+        response = {
+            "message": "Invalid number as addend",
+            "status": 400
+        }
+        json_response = json.dumps(response)
+        http_response = Response(response=json_response, status=400, mimetype="application/json")
+        return http_response
 
 @app.route('/newgame')
 def newgame():
-    global game                                                 # global variable - use and modify the game variable
-    game = Coregame(db_conf)
-    state = game.built_message()
-    json_response = json.dumps(state)
-    return Response(response=json_response, status=200, mimetype="application/json")
+    try:
+        global game                                                 # global variable - use and modify the game variable
+        game = Coregame(db_conf)
+        state = game.built_message()
+        json_response = json.dumps(state)
+        return Response(response=json_response, status=200, mimetype="application/json")
+    except ValueError:
+        response = {
+            "message": "Invalid number as addend",
+            "status": 400
+        }
+        json_response = json.dumps(response)
+        http_response = Response(response=json_response, status=400, mimetype="application/json")
+        return http_response
 
 @app.errorhandler(404)
 def page_not_found(error_code):
@@ -221,6 +266,11 @@ def page_not_found(error_code):
     json_response = json.dumps(response)
     http_response = Response(response=json_response, status=404, mimetype="application/json")
     return http_response
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "http://127.0.0.1:5501"
+    return response # Enable CORS for local frontend because got errors
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
