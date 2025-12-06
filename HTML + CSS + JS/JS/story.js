c31d98208aa526c8fc9219e55136d05d668bbfa8
Arsalan Khan <arsalan.khan.94@gmail.com>
2025-12-06 11:09:03 +0200

    change commit

--- HTML + CSS + JS/JS/story.js
+++ HTML + CSS + JS/JS/story.js
@@ -5,3 +5,8 @@ const playerName = localStorage.getItem("playerName");
 
 // Insert into text box span, our text story place
 document.getElementById("playerNameHere").textContent = playerName;
+
+// This is code to direct us to the gameplay.html //
+document.getElementById("continueBtn").addEventListener("click", function () {
+    window.location.href = "../HTML/gameplay.html";
+});
